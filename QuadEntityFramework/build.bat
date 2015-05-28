55e0c51b4caf8f7cfe9694240183d0c832e6d65f
Scubaboy <robin@robincawsey.plus.com>
2015-05-28 11:31:39 +0100

    added nuget restore to build file.

--- QuadEntityFramework/build.bat
+++ QuadEntityFramework/build.bat
@@ -12,7 +12,9 @@ set nuget=
 if "%nuget%" == "" (
 	set nuget=nuget
 )
- 
+
+%nuget% restore
+
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild QuadEntityFramework.sln /p:Configuration="%config%" /m /v:M /fl /flp:LogFile=msbuild.log;Verbosity=diag /nr:false
  
 mkdir Build
