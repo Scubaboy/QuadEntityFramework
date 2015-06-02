235014a2356b467b995013e53f41e704a443043e
Scubaboy <robin@robincawsey.plus.com>
2015-06-02 08:07:04 +0100

    added mapper classes

---
+++ QuadEntityFramework/Extensions/EntitiesMap.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using QuadEntityFramework.Entities;
+using QuadModels;
+
+namespace QuadEntityFramework.Extensions
+{
+    public static class EntitiesMap
+    {
+        static EntitiesMap()
+        {
+            Mapper.CreateMap<ActiveQuadEntity, ActiveQuad>()
+                .ConstructUsing(x => new ActiveQuad(x.ActiveQuadEntityId));
+            
+            Mapper.AssertConfigurationIsValid();
+        }
+
+        public static ActiveQuad ToModel(this ActiveQuadEntity activeQuad)
+        {
+            return activeQuad == null
+                ? null
+                : Mapper.Map<ActiveQuadEntity, ActiveQuad>(activeQuad);
+        }
+    }
+}
